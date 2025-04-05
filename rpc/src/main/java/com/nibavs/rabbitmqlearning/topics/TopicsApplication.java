3e83c0b9e24fb3d8c4d670bb9a0b1dd6b5acda1f
Nikita Bavs <nbavsunovsky@gmail.com>
2025-04-05 20:01:05 +0500

    RPC has been started

---
+++ rpc/src/main/java/com/nibavs/rabbitmqlearning/topics/TopicsApplication.java
@@ -0,0 +1,32 @@
+package com.nibavs.rabbitmqlearning.topics;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TopicsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TopicsApplication.class, args);
+	}
+
+	@Profile("usage_message")
+	@Bean
+	public CommandLineRunner usage() {
+		return args -> {
+			System.out.println("This app uses Spring Profiles to control its behavior.\n");
+			System.out.println("Sample usage: java -jar ./target/rabbit-tutorials.jar " +
+					"--spring.profiles.active=routing,sender");
+		};
+	}
+
+	@Profile("!usage_message")
+	@Bean
+	public CommandLineRunner tutorial() {
+		return new RabbitAmqpTutorialsRunner();
+	}
+}
